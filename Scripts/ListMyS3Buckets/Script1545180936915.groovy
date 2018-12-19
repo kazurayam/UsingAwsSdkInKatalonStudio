469b9ad0fb142aae264eb44f84a74fdafd135d57
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-12-19 10:50:43 +0900

    first commit

---
+++ Scripts/ListMyS3Buckets/Script1545180936915.groovy
@@ -0,0 +1,32 @@
+import com.amazonaws.ClientConfiguration
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+/**
+ * see "List Buckets" section in
+ * https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-s3-buckets.html
+ */
+
+final AmazonS3 s3 = AmazonS3ClientBuilder.
+						standard().
+						withClientConfiguration(getClientConfiguration()).
+						build();
+
+List<Bucket> buckets = s3.listBuckets();
+System.out.println("My Amazon S3 buckets are:");
+for (Bucket b : buckets) {
+	System.out.println("* " + b.getName());
+}
+
+
+/**
+ * with Proxy setting
+ * 
+ * @return
+ */
+ClientConfiguration getClientConfiguration() {
+	ClientConfiguration conf = new ClientConfiguration();
+	conf.setProxyHost("172.24.2.10");
+	conf.setProxyPort(8080);
+}
